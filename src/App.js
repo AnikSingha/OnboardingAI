cca92240a95d579c1a2089932be805ba89caf3d5
Anik Singha <aniksingha@outlook.com>
2024-10-20 17:49:00 -0400

    create page to reset password

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AIPrompts from './misc/AIPrompts.js';
 import SignUpPage from './routes/SignUpPage.js'; 
 import LoginPage from './routes/LoginPage';
 import ForgotPassword from './routes/ForgotPassword.js';
+import ResetPassword from './routes/ResetPassword.js';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="forgot-password" element={<ForgotPassword/>} />
+        <Route path="reset-password" element={<ResetPassword/>} />
 
       </Routes>
     </Router>

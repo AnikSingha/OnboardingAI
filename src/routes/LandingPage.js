8066cee2cd2a2b426521285e7450a974b5ab94a3
Anik Singha <aniksingha@outlook.com>
2024-10-21 17:27:22 -0400

    add fix for blinking on landing page

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -11,15 +11,18 @@ import { AuthContext } from '../AuthContext'
 
 export default function LandingPage() {
 
-  const { isAuthenticated } = useContext(AuthContext)
+  const { isAuthenticated, loading } = useContext(AuthContext)
   const navigate = useNavigate()
 
   useEffect(() => {
-    console.log(isAuthenticated)
     if (isAuthenticated) {
       navigate('/dashboard')
     }
   }, [isAuthenticated, navigate]);
+
+  if (loading) {
+    return <div className="min-h-screen bg-white"></div>
+  }
 
   return (
     <div className="flex flex-col min-h-screen bg-white font-sans">

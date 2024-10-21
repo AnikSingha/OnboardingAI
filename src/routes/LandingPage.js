83e0a2efd598f23556d8b1d4d4afb9d196b016a4
Anik Singha <aniksingha@outlook.com>
2024-10-21 17:37:42 -0400

    add fix for blinking to landing page

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../AuthContext';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
-import FeaturesSection from '../components/FeaturesSection.js';
-import CTASection from '../components/CTASection.js';
-import Footer from '../components/Footer.js';
-import { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../AuthContext'
-
+import FeaturesSection from '../components/FeaturesSection';
+import CTASection from '../components/CTASection';
+import Footer from '../components/Footer';
 
 export default function LandingPage() {
-
-  const { isAuthenticated } = useContext(AuthContext)
-  const navigate = useNavigate()
+  const { isAuthenticated, loading } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [localLoading, setLocalLoading] = useState(true);
 
   useEffect(() => {
-    console.log(isAuthenticated)
-    if (isAuthenticated) {
-      navigate('/dashboard')
+    if (!loading) {
+      if (isAuthenticated) {
+        navigate('/dashboard');
+      } else {
+        setLocalLoading(false);
+      }
     }
-  }, [isAuthenticated, navigate]);
+  }, [isAuthenticated, loading, navigate]);
+
+  if (localLoading || loading) {
+    return <div className="min-h-screen bg-white"></div>;
+  }
 
   return (
     <div className="flex flex-col min-h-screen bg-white font-sans">

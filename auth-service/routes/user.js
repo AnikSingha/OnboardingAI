d608f90f6d59c6e8dd72dfc96ae213cb6ecaf8a8
Anik Singha <aniksingha@outlook.com>
2024-10-27 17:29:47 -0400

    update user endpoints to send over new token with correct info

--- auth-service/routes/user.js
+++ auth-service/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const accountManager = require('../utils/accounts.js')
 const businessManager = require('../utils/businessManager.js')
-const { verifyToken } = require('../utils/token.js')
+const { verifyToken, createToken } = require('../utils/token.js')
 
 const router = express.Router({ strict: false });
 
@@ -38,6 +38,9 @@ router.put('/update-name', async (req, res) => {
         const success = await accountManager.updateUserName(email, name)
 
         if (success) {
+            const result = await accountManager.getUserInfo()
+            const token = createToken(result.name, email, result.business_name, result.role)
+            res.cookie('token', token, { httpOnly: true, sameSite: 'lax', secure: true,  maxAge: 86400000, domain: '.onboardingai.org' })
             return res.status(200).json({ success: true, message: 'Name successfully updated' })
         } else {
             return res.status(400).json({ success: false, message: 'Failed to update name' })
@@ -60,6 +63,9 @@ router.put('/update-email', async(req, res) => {
         const businessSuccess = await businessManager.updateEmployeeEmail(business_name, email, newEmail)
 
         if (userSuccess && businessSuccess) {
+            const result = await accountManager.getUserInfo()
+            const token = createToken(result.name, newEmail, result.business_name, result.role)
+            res.cookie('token', token, { httpOnly: true, sameSite: 'lax', secure: true,  maxAge: 86400000, domain: '.onboardingai.org' })
             return res.status(200).json({ success: true, message: 'Email successfully updated' })
         } else {
             return res.status(400).json({ success: false, message: 'Failed to update email' })

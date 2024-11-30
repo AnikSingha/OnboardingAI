0244572a7a683ebdd8a21f1dae743fb382d00eb0
Henry <henrytse2003@gmail.com>
2024-11-30 15:05:39 -0500

    changed endpoints for 2fa

--- src/routes/SettingsPage.js
+++ src/routes/SettingsPage.js
@@ -245,7 +245,7 @@ export default function SettingsPage() {
       } else {
         // Disable 2FA
         console.log('Disabling 2FA...');
-        const response = await fetch('https://api.onboardingai.org/auth/toggle-two-factor', {
+        const response = await fetch('https://api.onboardingai.org/auth/disable-two-factor', {
           method: 'POST',
           credentials: 'include',
           headers: {
